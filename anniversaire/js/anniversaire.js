ecb8fab1ab91fb81b6c546c7cb1d204723d6a15d
jerome-bouvier <moeb.bzhash@gmail.com>
2016-04-20 17:59:21 +0200

    fix end message

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,9 +1,14 @@
-$(function() {
-	var $bouton = $('#mainItem');
-
-	$bouton.on("click", function() {
-		var $cible = $('#firstMessage');
-		console.log("Clic");
-		$cible.css = ("visibility", "visible");
-	});
+$(function () {
+    var $bouton = $('#special');
+    var i = 0;
+    $bouton.on("click", function () {
+        i++;
+        if (i <= 4) {
+            console.log("Clic " + i + " fois");
+            $("li").eq(i).css("visibility", "visible");
+            if (i === 4) {
+                $("ul#messages li").text("HAPPY BIRTHDAY");
+            }
+        };
+    });
 });
