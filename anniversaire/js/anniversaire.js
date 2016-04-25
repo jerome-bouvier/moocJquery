75570cd04487a202903c97aa3a8932b75f972955
jerome-bouvier <moeb.bzhash@gmail.com>
2016-04-25 15:46:05 +0200

    qcm act2

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,16 +1,17 @@
 $(function () {
     var i = 1;
-    $('#special').on("click", function () {
-        $("li").eq(1).css("visibility", "visible");
+    $('#special').on('click', function () {
+        $('li').eq(1).css('visibility', 'visible');
+        $('span').css('visibility', 'hidden');
     });
 
-    $("ul#messages li").on("click", function () {
+    $('ul#messages li').on('click', function () {
         i++;
-        $("li").eq(i).css("visibility", "visible");
+        $('li').eq(i).css('visibility', 'visible');
         if (i === 4) {
-            $("ul#messages li").text("HAPPY BIRTHDAY");
-            $("body").css("background-image", "url");
-            $("sound").play();
+            $('ul#messages li').text('HAPPY BIRTHDAY');
+            $('body').css('background-image', 'url(./media/nyan.jpg)');
+            $('#sound')[0].play();
         }
     });
 });
---
+++ anniversaire/readme.txt
@@ -0,0 +1,11 @@
+hello,
+
+pas grand chose de transcendant c'est l'exemple donné pour 
+l'exercice à réaliser.
+
+Pour le code, je n'ai pas utilisé de variables seulement des sélections d'éléments. cela me semblait
+suffisant pour faire le job.
+
+Désolé pour le son, j'ai pris le premier fichier open source que j'avais sous la main.
+
+Jérôme
